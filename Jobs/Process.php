b0b1994bcbd0107092eca6e6eb64eec8a9affe0c
e <goldmouse67>
2016-09-28 22:18:30 -0400

    Updated path and added check to prevent dub key

--- Jobs/Process.php
+++ Jobs/Process.php
@@ -1,12 +1,16 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-include("./API.php");
+$path = str_replace("Jobs", "", __DIR__);
+include ($path."/IDB.php");
+include ($path."/MongoDBClient.php");
+include ($path."/API.php");
+
 $api = new API();
 $api->clearAllProducts();
 $api->loadProducts();
--- MongoDBClient.php
+++ MongoDBClient.php
@@ -11,57 +11,56 @@
  *
  * @author e
  */
-class MongoDBClient extends MongoClient implements IDB{
-    //put your code here
-   
-    public function collectionExists($collection){
-    
-        if ($this->system->namespaces->findOne(array('name'=>'eI_db.'.$collection)) === null){
-            return false;
-        }
-        return true;
-        
-    }
+class MongoDBClient extends MongoClient implements IDB {
+	//put your code here
 
-    public static function getInstance()
-    {
-        static $instance = null;
-        if (null === $instance) {
-            $instance = new self();
-        }
+	public function collectionExists($collection) {
 
-        return $instance;
-    }
+		if ($this->system->namespaces->findOne(array('name' => 'eI_db.'.$collection)) === null) {
+			return false;
+		}
+		return true;
 
+	}
 
-    public function getDB($name) {
-        return $this->selectDB($name);
-    }
+	public static function getInstance() {
+		static $instance = null;
+		if (null === $instance) {
+			$instance = new self();
+		}
 
-    public function setDB($name) {
-        
-    }
+		return $instance;
+	}
 
-    public function insertItems($items, $db,$table) {
-        $db = $this->getDB($db);
+	public function getDB($name) {
+		return $this->selectDB($name);
+	}
 
-        if ($this->collectionExists($table) === null) {
-            $collection = $db->createCollection($table);
-        } else {
-            $collection = $db->selectCollection($table);
-        }
+	public function setDB($name) {
 
-        foreach ($items as $k => $v) {
-            $collection->insert($v);
-        }
-    }
-    
-    public function removeAll($db,$table) {
-        $db = $this->getDB($db);
-        $collection = $db->selectCollection($table);
-        $collection->remove();
-    }
+	}
 
-  
+	public function insertItems($items, $db, $table) {
+		$db = $this->getDB($db);
+
+		if ($this->collectionExists($table) === null) {
+			$collection = $db->createCollection($table);
+		} else {
+			$collection = $db->selectCollection($table);
+		}
+
+		foreach ($items as $k   => $v) {
+			$q = array('productId' => $v->productId);
+			if (!$collection->findOne($q)) {
+				$collection->insert($v);
+			}
+		}
+	}
+
+	public function removeAll($db, $table) {
+		$db         = $this->getDB($db);
+		$collection = $db->selectCollection($table);
+		$collection->remove();
+	}
 
 }
